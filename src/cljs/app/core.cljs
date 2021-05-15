63faf0182abbe1a9e07338f4d78ab5ba74ad0d06
dmrd <git@ddohan.com>
2021-05-15 18:06:42 -0500

    Working canvas drawing

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,40 +1,127 @@
+
+; Using Refs: https://presumably.de/reagent-mysteries-part-3-manipulating-the-dom.html
 (ns app.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [clojure.string :as str]))
+            ["react" :as react]
+            [clojure.string :as str]
+            [goog.string :as gstring]
+            [goog.string.format]
+            ))
 
+; TODO: Switch to 1 db
+(defonce db_ref (r/atom {:canvas nil
+                         :context nil}))
 
-(defonce timer (r/atom (js/Date.)))
+(defonce canvas_ref (r/atom nil))
+(defonce context_ref (r/atom nil))
 
-(defonce time-color (r/atom "#f34"))
+; Maybe these should just be def-ed?
+(defonce isDrawing (r/atom false))
+;(def setIsDrawing [] (fn ) setIsDrawing (r/atom false))
 
-(defonce time-updater (js/setInterval
-                       #(reset! timer (js/Date.)) 1000))
+;(js/React.useState 0)
 
-(defn greeting [message]
-  [:h1 message])
+(defn startDrawing [event]
+  (let
+      [
+       event (.-nativeEvent event)
+       offsetX (.-offsetX event)
+       offsetY (.-offsetY event)
 
-(defn clock []
-  (let [time-str (-> @timer .toTimeString (str/split " ") first)]
-    [:div.example-clock
-     {:style {:color @time-color}}
-     time-str]))
+       ;canvas (.-canvas @context_ref)
+       canvas @context_ref
+       ;current (.-current context)
+        ]
+    ;(.beginPath (.-canvas canvas))
+    (.beginPath canvas)
+    (.moveTo canvas offsetX offsetY)
+    (reset! isDrawing true)
+    ;(set! (.-beginPath))
 
-(defn color-input []
-  [:div.color-input
-   "Time color: "
-   [:input {:type "text"
-            :value @time-color
-            :on-change #(reset! time-color (-> % .-target .-value))}]])
+    )
+  )
 
-(defn simple-example []
+(defn finishDrawing [event]
+  (reset! isDrawing false)
+  (let [canvas @context_ref
+        ]
+    (.closePath canvas)
+    )
+  )
+
+; QUESTION: How to get single value from map passed in?
+; QUESTION: How to effectively interop with js objects passed in?
+(defn draw [event]
+  (let
+      [
+       event (.-nativeEvent event)
+       offsetX (.-offsetX event)
+       offsetY (.-offsetY event)
+
+       canvas @context_ref
+       ;current (.-current canvas)
+       ;event (js->clj event)
+       ;n1 (get event "nativeEvent")
+       ;nativeEvent (.- event)
+       ]
+    (if @isDrawing
+      (do
+        (.lineTo canvas offsetX offsetY)
+        (.stroke canvas)
+        )
+
+      )
+  ))
+
+(defn canvas-pad []
+  (do
+  (react/useEffect
+   (fn [] (let [canvas @canvas_ref] ;(.-current @ref)
+               (set! (.. canvas -width) (* 2 (.-innerWidth js/window)))
+               (set! (.. canvas -height) (* 2 (.-innerHeight js/window)))
+               (set! (.. canvas -style -width) (gstring/format "%spx" (.-innerWidth js/window)))
+               (set! (.. canvas -style -height) (gstring/format "%spx" (.-innerHeight js/window)))
+               (reset! context_ref (.getContext canvas "2d"))
+               (.scale @context_ref 2 2)
+               (set! (.-lineCap @context_ref) "round")
+               (set! (.-strokeStyle @context_ref) "black")
+               (set! (.-lineWidth @context_ref) 5)
+           ))
+   #js []
+  )
   [:div
-   [greeting "Hello world, it is now"]
-   [clock]
-   [color-input]])
+   [:h1 "Canvas drawing"]
+   [:div "Draw with your mouse."]
+   [:canvas
+    {
+     :onMouseDown startDrawing
+     :onMouseUp finishDrawing
+     :onMouseMove draw
+     :ref (fn [el] (reset! canvas_ref el) )
+
+     }
+
+    ]])
+  )
+
+(defn main []
+  [canvas-pad]
+)
+
+
 
 (defn run []
-  (rdom/render [simple-example] (js/document.getElementById "root")))
+  ;(rdom/render [canvas-pad]
+  (let [div  (js/document.getElementById "root")]
+  (def functional-compiler (reagent.core/create-compiler {:function-components true}))
+
+  ;; Using the option
+  (reagent.dom/render [main] div functional-compiler)
+  (reagent.core/as-element [main] functional-compiler)
+  ;; Setting compiler as the default
+  (reagent.core/set-default-compiler! functional-compiler)
+  ))
 
 
 ;; Exported methods
